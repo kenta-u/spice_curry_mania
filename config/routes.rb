d9efe937282a4023a40d59040a4423f6ed1c46a6
kenta uchinokura <kenta@kenta-no-MacBook-Pro.local>
2021-12-15 12:46:46 +0900

    add: ルーティングの追加 #8

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users, only: %i[new create edit update destroy] do
+    get :favorites, on: :collection
+  end
+  resources :recipes, only: %i[new create index edit update destroy] do
+    resources :favorites, only: %i[create destroy]
+  end
+  
+  namespace :reicpes do
+    resources :serches, only: %i[index]
+  end
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
+  root 'resipes#index'
 end
