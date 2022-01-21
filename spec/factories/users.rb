4c6b83849c0b60d4319c6fb2f7abc2b1d9e00b3a
kenta uchinokura <kenta@kenta-no-MacBook-Pro.local>
2022-01-21 18:41:47 +0900

    FactoryBotにuserの属性を定義 #70

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :user do
-    
+    name { "test" }
+    sequence(:email) { |n| "test-#{n}@example.com" }
+    password { "12345678" }
+    password_confirmation { "12345678" }
   end
 end
