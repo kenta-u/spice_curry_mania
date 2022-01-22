a57135f12e074a920c2359eed975bc72f0cf938a
kenta uchinokura <kenta@kenta-no-MacBook-Pro.local>
2022-01-22 17:34:50 +0900

    ingredientのFactoryBotとモデルスペックを追加 #70

--- spec/models/ingredient_spec.rb
+++ spec/models/ingredient_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Ingredient, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validate' do
+    it '材料名と分量がある場合、有効である' do
+      ingredient = build(:ingredient)
+      expect(ingredient).to be_valid
+      expect(ingredient.errors).to be_empty
+    end
+
+    it '材料名がない場合、無効である' do
+      ingredient = build(:ingredient, name: "")
+      expect(ingredient).to be_invalid
+      expect(ingredient.errors[:name]).to include("を入力してください")
+    end
+
+    it '分量がない場合、無効である' do
+      ingredient = build(:ingredient, quantity: "")
+      expect(ingredient).to be_invalid
+      expect(ingredient.errors[:quantity]).to include("を入力してください")
+    end
+  end
 end
